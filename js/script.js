b7a10e715d4ad253473c91a75202184f52ead179
Cesar Abel Perez Martinez <15460531@colima.tecnm.mx>
2021-06-28 00:29:55 -0500

    Se agregaron funciones basicas

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function ComisionCredito() {
+    var valor = document.getElementById("inputCredito").value;
+    document.getElementById("labelComisionCredito").innerHTML = Number.parseFloat(calcularComision(valor, 1.86)).toFixed(2);
+    document.getElementById("labelTotalCredito").innerHTML = Number.parseFloat(calcularComision(valor, 1.86) + parseInt(valor)).toFixed(2);
+}
+
+function ComisionDebito() {
+    var valor = document.getElementById("inputDebito").value;
+    document.getElementById("labelComisionDebito").innerHTML = Number.parseFloat(calcularComision(valor, 2.21)).toFixed(2);
+    document.getElementById("labelTotalDebito").innerHTML = Number.parseFloat(calcularComision(valor, 2.21) + parseInt(valor)).toFixed(2);
+}
+
+function calcularComision(valor, comision) {
+    return comision * (valor / 100);
+
+}
